89b975213bec461f36919c7a37194e32183b963c
taigatomita <tiger101837@gmail.com>
2022-10-14 02:39:51 +0900

    ESLint,prettierの追加及びコードの整形

--- ToDo_app/js/main.js
+++ ToDo_app/js/main.js
@@ -1,40 +1,43 @@
+// eslint-disable-next-line no-undef, no-unused-vars
 const app = new Vue({
   el: "#ToDoApp",
-  data: {
-    textInput: '',
-    todos: [],
-    formState: 'add',
-    indexOfTodoToChange: 0
+  data: function () {
+    return {
+      textInput: "",
+      todos: [],
+      formState: "add",
+      indexOfTodoToChange: 0,
+    };
   },
   mounted() {
-    this.todos = JSON.parse(localStorage.getItem('TodoList')) || []
+    this.todos = JSON.parse(localStorage.getItem("TodoList")) || [];
   },
   methods: {
     saveTodo() {
-      localStorage.setItem('TodoList', JSON.stringify(this.todos))
+      localStorage.setItem("TodoList", JSON.stringify(this.todos));
     },
     addTodo() {
-      const todo = { text: this.textInput }
-      this.todos.push(todo)
-      this.saveTodo()
-      this.textInput = ''
+      const todo = { text: this.textInput };
+      this.todos.push(todo);
+      this.saveTodo();
+      this.textInput = "";
     },
     deleteTodo(index) {
-      this.todos.splice(index, 1)
-      this.saveTodo()
+      this.todos.splice(index, 1);
+      this.saveTodo();
     },
     changeToEditForm(index) {
-      this.formState = 'edit'
-      this.indexOfTodoToChange = index
-      this.textInput = this.todos[index].text
+      this.formState = "edit";
+      this.indexOfTodoToChange = index;
+      this.textInput = this.todos[index].text;
     },
     editTodo() {
-      const todo = { text: this.textInput}
-      this.todos[this.indexOfTodoToChange] = todo
-      this.saveTodo()
-      this.formState = 'add'
-      this.indexOfTodoToChange = 0
-      this.textInput = ''
-    }
-  }
-})
+      const todo = { text: this.textInput };
+      this.todos[this.indexOfTodoToChange] = todo;
+      this.saveTodo();
+      this.formState = "add";
+      this.indexOfTodoToChange = 0;
+      this.textInput = "";
+    },
+  },
+});
